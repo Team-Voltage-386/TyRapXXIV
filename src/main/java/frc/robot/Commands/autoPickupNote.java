3700270d04cb9801805f3f1c390a434e853deeb9
LucasCodee <lucascornejo222@gmail.com>
2024-02-29 20:36:47 -0500

    updated autopickup

--- src/main/java/frc/robot/Commands/autoPickupNote.java
+++ src/main/java/frc/robot/Commands/autoPickupNote.java
@@ -1,24 +1,16 @@
 package frc.robot.Commands;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
-import frc.robot.Constants.Controller;
-import frc.robot.Constants.Deadbands;
 import frc.robot.Subsystems.Drivetrain;
 import frc.robot.Utils.Aimlock;
+import frc.robot.Utils.Flags;
 import frc.robot.Utils.LimelightHelpers;
+import frc.robot.Utils.Flags.subsystemsStates;
 
 public class autoPickupNote extends Command {
     Drivetrain dt;
-    private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(Controller.kRateLimitXSpeed);
-    private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(Controller.kRateLimitYSpeed);
-    private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(Controller.kRateLimitRot);
 
     private double start;
 
@@ -41,18 +33,20 @@ public class autoPickupNote extends Command {
     private void readControllers() {
         // Get the x speed. We are inverting this because Xbox controllers return
         // negative values when we push forward.
-        xSpeed = Aimlock.hasTarget() ? 2 : 0;
+        xSpeed = Aimlock.hasTarget() ? 1 : 0;
+        ySpeed = Aimlock.hasTarget() ? MathUtil.clamp(LimelightHelpers.getTY("limelight-c") / 20, 0, 1) : 0;
     }
 
     @Override
     public void execute() {
         readControllers();
-        dt.lockPiece(xSpeed, 0, 0, false, true);
+        dt.lockPiece(xSpeed, ySpeed, 0, false, false);
     }
 
     @Override
     public boolean isFinished() {
         return Timer.getFPGATimestamp() > (start + 1.25)
-                || (LimelightHelpers.getTY("limelight-a") > -8 && Aimlock.hasTarget());
+                || (LimelightHelpers.getTY("limelight-a") > -8 && Aimlock.hasTarget())
+                || Flags.pieceState.equals(subsystemsStates.holdingPiece);
     }
 }
