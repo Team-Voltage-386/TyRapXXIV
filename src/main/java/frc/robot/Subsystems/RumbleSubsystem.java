f5b2699328f76f579c2f6a49b467dd54bdb01c48
RyanCofer <rcofer56@gmail.com>
2024-03-09 13:42:40 -0500

    Merged rumble control
    Now has rumble codes for manipulator tryouts.

--- src/main/java/frc/robot/Subsystems/RumbleSubsystem.java
+++ src/main/java/frc/robot/Subsystems/RumbleSubsystem.java
@@ -4,13 +4,7 @@
 
 package frc.robot.Subsystems;
 
-import java.util.Map;
-
-import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
