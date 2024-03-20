484cf75893a2930b6ce3183ebb12740269872447
RyanCofer <rcofer56@gmail.com>
2024-03-19 20:45:42 -0400

    Fix^7

--- src/main/java/frc/robot/Commands/TargetAquiredLEDCommand.java
+++ src/main/java/frc/robot/Commands/TargetAquiredLEDCommand.java
@@ -35,6 +35,6 @@ public class TargetAquiredLEDCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return LimelightHelpers.getTV("limelight-b");
+    return !LimelightHelpers.getTV("limelight-b");
   }
 }
