cc7d93666faa8cefa3f0be2b5172c4d097b189b4
c5anchez <csanchez@alum.mit.edu>
2025-02-01 15:26:42 -0500

    Updated max speed constants in DriveTrain.

--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -41,8 +41,8 @@ import frc.robot.SwerveModule;
 /** Represents a swerve drive style drivetrain. */
 public class Drivetrain extends SubsystemBase {
     
-    public static final double kMaxPossibleSpeed = 4.0;
-    public static final double kMaxAngularSpeed = 1.5 * Math.PI; // per second
+    public static final double kMaxPossibleSpeed = 5.0;
+    public static final double kMaxAngularSpeed = 3 * Math.PI; // per second
 
     private final Translation2d m_frontLeftLocation = new Translation2d(
             DriveTrain.kDistanceMiddleToFrontMotor * DriveTrain.kXForward,
